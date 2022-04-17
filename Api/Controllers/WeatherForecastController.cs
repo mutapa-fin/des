8ee1ddf012f3a6dbe89bb09ba181c4582ba3bc04
Kudzaishe Zharare <kudzicloud@gmail.com>
2022-04-17 13:42:45 +0200

    first commit

---
+++ Api/Controllers/WeatherForecastController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        public static string Encrypt(string plainText, string key)
+        {
+            TripleDESCryptoServiceProvider desCryptoProvider = new
+            TripleDESCryptoServiceProvider();
+            MD5CryptoServiceProvider hashMD5Provider = new MD5CryptoServiceProvider();
+            byte[] byteHash;
+            byte[] byteBuff;
+            byteHash = hashMD5Provider.ComputeHash(Encoding.UTF8.GetBytes(key));
+            desCryptoProvider.Key = byteHash;
+            desCryptoProvider.Mode = CipherMode.ECB;
+            byteBuff = Encoding.UTF8.GetBytes(plainText);
+            string encoded =
+            Convert.ToBase64String(desCryptoProvider.CreateEncryptor().TransformFinalBlock(byteBuff, 0, byteBuff.Length));
+            return encoded;
+        }
+        public static string Decrypt(string encodedText, string key)
+        {
+            TripleDESCryptoServiceProvider desCryptoProvider = new
+            TripleDESCryptoServiceProvider();
+            MD5CryptoServiceProvider hashMD5Provider = new MD5CryptoServiceProvider();
+            byte[] byteHash;
+            byte[] byteBuff;
+            byteHash = hashMD5Provider.ComputeHash(Encoding.UTF8.GetBytes(key));
+            desCryptoProvider.Key = byteHash;
+            desCryptoProvider.Mode = CipherMode.ECB;
+            byteBuff = Convert.FromBase64String(encodedText);
+            string plaintext =
+            Encoding.UTF8.GetString(desCryptoProvider.CreateDecryptor().TransformFinalBlock(byteBuff, 0, byteBuff.Length));
+            return plaintext;
+        }
+
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public IEnumerable<WeatherForecast> Post([FromBody] WeatherForecast weather)
+        {
+            string pk = "pYsqtHtuqAa8QoWZyUwP3vMTCCu2tMNZ";
+            int command = weather.Command;
+            string message = weather.Message;
+
+            if (command == 1)
+            {
+                return Enumerable.Range(1, 1).Select(index => new WeatherForecast
+                {
+                    Message = Decrypt(message, pk)
+                })
+            .ToArray();
+            }
+            else
+            {
+                return Enumerable.Range(1, 1).Select(index => new WeatherForecast
+                {
+                    Message = Encrypt(message, pk)
+                })
+            .ToArray();
+            }
+           
+            
+
+            
+        }
+    }
+}
---
+++ Api/WeatherForecast.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Api
+{
+    public class WeatherForecast
+    {
+        public int Command { get; set; }
+        public string Message { get; set; }
+    }
+}
